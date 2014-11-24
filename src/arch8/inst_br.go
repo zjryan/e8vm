eb2d22c48f73840e1182b36307fba68970344b28
He Liu <liulonnie@gmail.com>
2014-11-24 03:22:52 -0800

    branch and immediate instruction

---
+++ src/arch8/inst_br.go
@@ -0,0 +1,34 @@
+package arch8
+
+// InstBr exectues branch instruction
+type InstBr struct{}
+
+// I executes the instruction.
+// Might return invalid instruction exception.
+func (i *InstBr) I(cpu *CPU, in uint32) *Excep {
+	op := (in >> 24) & 0xff  // (32:24]
+	src1 := (in >> 21) & 0x7 // (24:21]
+	src2 := (in >> 18) & 0x7 // (21:18]
+	im := in & 0x3ffff       // (18:0]
+
+	s1 := cpu.regs[src1]
+	s2 := cpu.regs[src2]
+	pc := cpu.regs[PC]
+	br := pc + uint32(int32(im<<14)>>12)
+
+	switch op {
+	case 32: // bne
+		if s1 != s2 {
+			pc = br
+		}
+	case 33: // beq
+		if s1 == s2 {
+			pc = br
+		}
+	default:
+		return errInvalidInst
+	}
+
+	cpu.regs[PC] = pc
+	return nil
+}
---
+++ src/arch8/inst_imm.go
@@ -0,0 +1,61 @@
+package arch8
+
+// InstImm executes immediate instructions
+type InstImm struct{}
+
+// I executes the instruction.
+// Might return invalid instruction exception,
+// or memory related exceptions.
+func (i *InstImm) I(cpu *CPU, in uint32) *Excep {
+	op := (in >> 24) & 0xff  // (32:24]
+	dest := (in >> 21) & 0x7 // (24:21]
+	src := (in >> 18) & 0x7  // (21:18]
+	im := in & 0xffff        // (16:0]
+
+	s := cpu.regs[src]
+	d := cpu.regs[dest]
+	ims := uint32(int32(in<<16) >> 16)
+	addr := s + ims
+	var e *Excep
+	var b byte
+
+	switch op {
+	case 0:
+		panic("register based instruction")
+	case 1: // addi
+		d = s + ims
+	case 2: // slti
+		if int32(s) < int32(ims) {
+			d = 1
+		} else {
+			d = 0
+		}
+	case 3: // andi
+		d = s & im
+	case 4: // ori
+		d = s | im
+	case 5: // lui
+		d = im << 16
+	case 6: // lw
+		d, e = cpu.virtMem.ReadWord(addr)
+	case 7: // lb
+		b, e = cpu.virtMem.ReadByte(addr)
+		d = uint32(int32(int8(b)))
+	case 8: // lbu
+		b, e = cpu.virtMem.ReadByte(addr)
+		d = uint32(b)
+	case 9: // sw
+		e = cpu.virtMem.WriteWord(addr, d)
+	case 10: // sb
+		e = cpu.virtMem.WriteByte(addr, byte(d))
+	default:
+		return errInvalidInst
+	}
+
+	if e != nil {
+		return e
+	}
+
+	cpu.regs[dest] = d
+	return nil
+}
