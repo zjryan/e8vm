66cd53d57a83d0013ae896d6d664b9ccc3d83ff2
He Liu <liulonnie@gmail.com>
2014-11-24 00:54:19 -0800

    floating

--- src/arch8/inst_reg.go
+++ src/arch8/inst_reg.go
@@ -1,5 +1,9 @@
 package arch8
 
+import (
+	"math"
+)
+
 // InstReg executes register based instructions
 type InstReg struct{}
 
@@ -14,79 +18,100 @@ func (i *InstReg) I(cpu *CPU, in uint32) *Excep {
 	src1 := (in >> 18) & 0x7
 	src2 := (in >> 15) & 0x7
 	shift := (in >> 10) & 0x1f
-	funct := in & 0x3ff
+	isFloat := (in >> 8) & 0x1
+	funct := in & 0xff
 
 	s1 := cpu.regs[src1]
 	s2 := cpu.regs[src2]
 	d := uint32(0)
 
-	switch funct {
-	case 0: // sll
-		d = s1 << shift
-	case 1: // srl
-		d = s1 >> shift
-	case 2: // sra
-		d = uint32(int32(s1) >> shift)
-	case 3: // sllv
-		d = s1 << s2
-	case 4: // srlv
-		d = s1 >> s2
-	case 5: // srla
-		d = uint32(int32(s1) >> s2)
-	case 6: // add
-		d = s1 + s2
-	case 7: // sub
-		d = s1 - s2
-	case 8: // and
-		d = s1 & s2
-	case 9: // or
-		d = s1 | s2
-	case 10: // xor
-		d = s1 ^ s2
-	case 11: // nor
-		d = ^(s1 | s2)
-	case 12: // slt
-		if int32(s1) < int32(s2) {
-			d = 1
-		} else {
-			d = 0
+	if isFloat == 0 {
+		switch funct {
+		case 0: // sll
+			d = s1 << shift
+		case 1: // srl
+			d = s1 >> shift
+		case 2: // sra
+			d = uint32(int32(s1) >> shift)
+		case 3: // sllv
+			d = s1 << s2
+		case 4: // srlv
+			d = s1 >> s2
+		case 5: // srla
+			d = uint32(int32(s1) >> s2)
+		case 6: // add
+			d = s1 + s2
+		case 7: // sub
+			d = s1 - s2
+		case 8: // and
+			d = s1 & s2
+		case 9: // or
+			d = s1 | s2
+		case 10: // xor
+			d = s1 ^ s2
+		case 11: // nor
+			d = ^(s1 | s2)
+		case 12: // slt
+			if int32(s1) < int32(s2) {
+				d = 1
+			} else {
+				d = 0
+			}
+		case 13: // sltu
+			if s1 < s2 {
+				d = 1
+			} else {
+				d = 0
+			}
+		case 14: // mul
+			d = uint32(int32(s1) * int32(s2))
+		case 15: // mulu
+			d = s1 * s2
+		case 16: // div
+			if s2 == 0 {
+				d = 0
+			} else {
+				d = uint32(int32(s1) / int32(s2))
+			}
+		case 17: // divu
+			if s2 == 0 {
+				d = 0
+			} else {
+				d = s1 / s2
+			}
+		case 18: // mod
+			if s2 == 0 {
+				d = 0
+			} else {
+				d = uint32(int32(s1) % int32(s2))
+			}
+		case 19: // modu
+			if s2 == 0 {
+				d = 0
+			} else {
+				d = s1 % s2
+			}
+		default:
+			return errInvalidInst
 		}
-	case 13: // sltu
-		if s1 < s2 {
-			d = 1
-		} else {
-			d = 0
+	} else {
+		f1 := math.Float32frombits(s1)
+		f2 := math.Float32frombits(s2)
+		var fd float32
+		switch funct {
+		case 0: // fadd
+			fd = f1 + f2
+		case 1: // fsub
+			fd = f1 - f2
+		case 2: // fmul
+			fd = f1 * f2
+		case 3: // fdiv
+			fd = f1 / f2
+		default:
+			return errInvalidInst
 		}
-	case 14: // mul
-		d = uint32(int32(s1) * int32(s2))
-	case 15: // mulu
-		d = s1 * s2
-	case 16: // div
-		if s2 == 0 {
-			d = 0
-		} else {
-			d = uint32(int32(s1) / int32(s2))
-		}
-	case 17: // divu
-		if s2 == 0 {
-			d = 0
-		} else {
-			d = s1 / s2
-		}
-	case 18: // mod
-		if s2 == 0 {
-			d = 0
-		} else {
-			d = uint32(int32(s1) % int32(s2))
-		}
-	case 19: // modu
-		if s2 == 0 {
-			d = 0
-		} else {
-			d = s1 % s2
-		}
-	default:
-		return errInvalidInst
+
+		d = math.Float32bits(fd)
 	}
 
 	cpu.regs[dest] = d
