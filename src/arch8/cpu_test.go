2b150b8383c2925afa5668421c47e73e333366d0
He Liu <liulonnie@gmail.com>
2014-11-27 14:44:49 -0800

    interrupt test

--- src/arch8/cpu_test.go
+++ src/arch8/cpu_test.go
@@ -18,14 +18,37 @@ func (i ti1) I(cpu *CPU, in uint32) *Excep {
 }
 
 func TestCPU(t *testing.T) {
-	as := func(cond bool) {
+	as := func(cond bool, s string, args ...interface{}) {
 		if !cond {
-			t.Fail()
+			t.Fatalf(s, args...)
 		}
 	}
 
 	m := NewPhyMemory(PageSize * 32)
 	cpu := NewCPU(m, ti1(0), 0)
 	e := cpu.Tick()
-	as(e == errHalt)
+	as(e == errHalt, "not halting")
+
+	cpu.Reset()
+	m.WriteWord(InitPC, 1)
+	e = cpu.Tick()
+	as(e == errTimeInt, "not time interrupt")
+
+	cpu.Reset()
+	cpu.interrupt.Enable()
+	cpu.interrupt.EnableInt(errTimeInt.Code)
+	cpu.interrupt.writeWord(intKernelSP, 0x10000)  // page 16
+	cpu.interrupt.writeWord(intHandlerPC, 0x10000) // page 16
+
+	e = cpu.Tick()
+	as(e == nil, "should have no error")
+	as(cpu.regs[PC] == 0x10000, "pc incorrect")
+	as(cpu.regs[SP] == 0x10000, "sp incorrect")
+	as(cpu.regs[RET] == 0x8000, "ret incorrect")
+	b, e := m.ReadByte(0x10000 - intFrameSize + intFrameCode)
+	as(e == nil, "read byte error")
+	as(b == errTimeInt.Code, "not time interrupt")
+
+	e = cpu.Tick()
+	as(e == errHalt, "should halt")
 }
