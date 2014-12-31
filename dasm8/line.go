ad412dae2a7b360c8f1dd2ed195218e2b8e67f4b
He Liu <liulonnie@gmail.com>
2014-12-31 02:14:09 -0800

    adding ascii in dasm

--- dasm8/line.go
+++ dasm8/line.go
@@ -1,6 +1,7 @@
 package dasm8
 
 import (
+	"bytes"
 	"fmt"
 )
 
@@ -16,21 +17,30 @@ type Line struct {
 	ToLine *Line
 }
 
+func printables(inst uint32) string {
+	ret := ""
+	for i := 0; i < 4; i++ {
+		r := rune(inst & 0xff)
+		inst = inst >> 8
+		if r >= ' ' && r <= '~' {
+			ret = string(r) + ret
+		} else {
+			ret = "." + ret
+		}
+	}
+	return ret
+}
+
 func (line *Line) String() string {
-	if !line.IsJump {
-		return fmt.Sprintf("%08x:  %08x     %s",
-			line.Addr,
-			line.Inst,
-			line.Str,
-		)
+	ret := new(bytes.Buffer)
+	fmt.Fprintf(ret, "%08x:  %08x", line.Addr, line.Inst)
+	fmt.Fprintf(ret, "   %s", printables(line.Inst))
+	fmt.Fprintf(ret, "    %s", line.Str)
+	if line.IsJump {
+		fmt.Fprintf(ret, "   // %08x", line.To)
 	}
 
-	return fmt.Sprintf("%08x:  %08x     %s    // %08x",
-		line.Addr,
-		line.Inst,
-		line.Str,
-		line.To,
-	)
+	return ret.String()
 }
 
 func newLine(addr uint32, in uint32) *Line {
