cb0aee738d89f38d7a3afea8ffd8f2edec1c4705
He Liu <liulonnie@gmail.com>
2015-02-15 19:29:47 -0800

    e8t

--- e8t/main.go
+++ e8t/main.go
@@ -2,21 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"lonnie.io/e8vm/fs8"
 )
 
-func ne(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
 func main() {
-	ret, e := fs8.AllPackages(".")
-	ne(e)
-
-	for _, p := range ret {
-		fmt.Println(p)
-	}
+	fmt.Println("e8vm testing program")
 }
