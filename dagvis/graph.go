b149420137a5a23b56a80bd2727b1bcd33974b91
He Lonnie Liu <liulonnie@gmail.com>
2015-02-20 13:30:31 -0800

    moving dagvis package into another repo

--- dagvis/graph.go
+++
@@ -1,8 +0,0 @@
-// Package dagvis visualizes a DAG graph into
-// a structured, layered planer map.
-package dagvis
-
-// Graph is a directed graph
-type Graph struct {
-	Nodes map[string][]string
-}
