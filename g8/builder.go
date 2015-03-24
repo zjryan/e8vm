2bbf56985e688788f717cae910b81051312b4d29
He Liu <liulonnie@gmail.com>
2015-03-24 02:07:54 -0700

    start g8 builder

--- g8/builder.go
+++ g8/builder.go
@@ -1,4 +1,9 @@
 package g8
 
+import (
+	"lonnie.io/e8vm/g8/ir"
+)
+
 type builder struct {
+	curFunc *ir.Func
 }
---
+++ g8/ir/ir.go
@@ -0,0 +1,14 @@
+package ir
+
+// Stmt is an ir statement
+type Stmt struct {
+}
+
+// Block is a basic block
+type Block struct {
+	id int // basic block id
+}
+
+// Func is an intermediate function
+type Func struct {
+}
