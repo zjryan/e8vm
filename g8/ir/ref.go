2e4703b04c216bf7fe645cfd52b67e44a3fccf3f
He Liu <liulonnie@gmail.com>
2015-03-25 00:08:11 -0700

    ir

--- g8/ir/ref.go
+++ g8/ir/ref.go
@@ -4,9 +4,13 @@ type ref interface{}
 
 // stackVar is a variable on stack
 type stackVar struct {
-	name         string
-	id           int
-	offset, size int32
+	name   string
+	id     int
+	offset int32
+	size   int32
+
+	// regOnly stack vars does not take frame space on the stack
+	regOnly bool
 }
 
 type heapVar struct{ pkg, sym int } // a variable symbol on heap
