246b63fd720074f330d79fc1b464ff0b22883ad0
He Liu <liulonnie@gmail.com>
2015-03-30 10:30:58 -0700

    .

--- g8/builtin.go
+++ g8/builtin.go
@@ -1,18 +1,14 @@
 package g8
 
 import (
-	"os"
-
 	"lonnie.io/e8vm/link8"
 )
 
 func declareBuiltin(b *builder, builtin *link8.Pkg) {
-	builtin.PrintSymbols(os.Stdout)
-
 	o := func(name string, as string) {
 		sym, index := builtin.SymbolByName(name)
 		if sym == nil {
-			b.Errorf(nil, "%s not found in builtin package", name)
+			b.Errorf(nil, "builtin symbol %s missing", name)
 			return
 		}
 
